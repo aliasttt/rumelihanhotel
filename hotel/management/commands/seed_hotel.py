416401e7bfad786fdceff7582e20fabc8d558c43
ali <aliasadi3853@gmail.com>
2026-06-24 21:40:25 +0330

    first commit

--- hotel/management/commands/seed_hotel.py
+++ hotel/management/commands/seed_hotel.py
@@ -75,15 +75,18 @@ class Command(BaseCommand):
 
         settings = SiteSettings.get_solo()
         settings.hotel_name = "Rumelihan Hotel"
-        settings.phone = "+90 212 000 00 00"
-        settings.whatsapp = "902120000000"
+        settings.phone = "+90 541 122 67 05"
+        settings.whatsapp = "905411226705"
         settings.email = "info@rumelihanhotel.com"
         settings.address_tr = "Beyoğlu, İstanbul. İstiklal Caddesi ve Taksim'e yürüme mesafesinde."
         settings.address_en = "Beyoglu, Istanbul. Within walking distance of Istiklal Street and Taksim."
         settings.instagram_url = "https://www.instagram.com/"
-        settings.google_maps_link = "https://maps.google.com/"
+        settings.google_maps_link = "https://www.google.com/maps/search/?api=1&query=%C5%9Eehit%20Muhtar%2C%20%C4%B0stiklal%20Cd.%20No%3A48%2C%2034435%20Beyo%C4%9Flu%2F%C4%B0stanbul"
+        settings.google_maps_embed = '<iframe src="https://maps.google.com/maps?q=%C5%9Eehit%20Muhtar%2C%20%C4%B0stiklal%20Cd.%20No%3A48%2C%2034435%20Beyo%C4%9Flu%2F%C4%B0stanbul&output=embed" loading="lazy" referrerpolicy="no-referrer-when-downgrade" allowfullscreen></iframe>'
         settings.footer_text_tr = "Beyoğlu'nun kalbinde tarihi butik otel konforu."
         settings.footer_text_en = "Historic boutique hotel comfort in the heart of Beyoglu."
+        settings.address_tr = "Şehit Muhtar, İstiklal Cd. No:48, 34435 Beyoğlu/İstanbul"
+        settings.address_en = "Şehit Muhtar, İstiklal Cd. No:48, 34435 Beyoğlu/Istanbul"
         settings.save()
 
         HeroSlide.objects.update_or_create(
---
+++ hotel/management/commands/set_site_contact.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+
+from hotel.models import SiteSettings
+
+
+class Command(BaseCommand):
+    help = "Update Rumelihan Hotel contact, WhatsApp, address and map settings."
+
+    def handle(self, *args, **options):
+        settings = SiteSettings.get_solo()
+        settings.phone = "+90 541 122 67 05"
+        settings.whatsapp = "905411226705"
+        settings.address_tr = "Şehit Muhtar, İstiklal Cd. No:48, 34435 Beyoğlu/İstanbul"
+        settings.address_en = "Şehit Muhtar, İstiklal Cd. No:48, 34435 Beyoğlu/Istanbul"
+        settings.google_maps_link = (
+            "https://www.google.com/maps/search/?api=1&query="
+            "%C5%9Eehit%20Muhtar%2C%20%C4%B0stiklal%20Cd.%20No%3A48%2C%2034435%20Beyo%C4%9Flu%2F%C4%B0stanbul"
+        )
+        settings.google_maps_embed = (
+            '<iframe src="https://maps.google.com/maps?q='
+            "%C5%9Eehit%20Muhtar%2C%20%C4%B0stiklal%20Cd.%20No%3A48%2C%2034435%20Beyo%C4%9Flu%2F%C4%B0stanbul"
+            '&output=embed" loading="lazy" referrerpolicy="no-referrer-when-downgrade" allowfullscreen></iframe>'
+        )
+        settings.save()
+        self.stdout.write(self.style.SUCCESS("Site contact information updated."))
